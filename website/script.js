2313d4a14f4b85c516f34a38d935d0a05cccc4c2
OliverCrowe <oliver.crowe@hotmail.co.uk>
2020-01-19 11:32:21 +0000

    added login form listener and eslint

--- website/script.js
+++ website/script.js
@@ -2,3 +2,12 @@
 $(window).on('load',function(){
     $('#login_box').modal('show');
 });
+
+function logIn(event) {
+    event.preventDefault();
+    $('#login_box').modal('hide');
+}
+
+document.addEventListener('DOMContentLoaded', function() { //set up listeners
+    document.getElementById('login_form').addEventListener('submit', logIn );
+});
