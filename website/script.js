a9652d417dd9875f33d8257f660e7f95c5399c57
OliverCrowe <oliver.crowe@hotmail.co.uk>
2020-01-19 11:47:37 +0000

    added user and content editing buttons

--- website/script.js
+++ website/script.js
@@ -3,11 +3,25 @@ $(window).on('load',function(){
     $('#login_box').modal('show');
 });
 
-function logIn(event) {
+function logIn(event) { //the login function currently only hides the login box
     event.preventDefault();
     $('#login_box').modal('hide');
 }
 
+function userClick(event) {
+    event.preventDefault();
+    document.getElementById('main_content').innerHTML = '<p>The users area is loaded</p>';
+    //the code for loading the user modification area goes here
+}
+
+function contentClick(event) {
+    event.preventDefault();
+    document.getElementById('main_content').innerHTML = '<p>The content area is loaded</p>';
+    //the code for loading the content modification area goes here
+}
+
 document.addEventListener('DOMContentLoaded', function() { //set up listeners
     document.getElementById('login_form').addEventListener('submit', logIn );
+    document.getElementById('users').addEventListener('click', userClick );
+    document.getElementById('content').addEventListener('click', contentClick );
 });
