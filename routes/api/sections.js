e2e1a664fabea7eddf89dc6d8dfdbccc8e86f087
OliverCrowe <oliver.crowe@hotmail.co.uk>
2020-01-23 21:49:35 +0000

    nearly done with section/add route

--- routes/api/sections.js
+++ routes/api/sections.js
@@ -40,15 +40,21 @@ router.get('/', (req, res) => {
 });
 
 router.post('/add', upload.array('section_files[]', 20), (req, res) => {
-    let sectionTitle = req.body.sectionTitle;
-    let sectionText = req.body.sectionText;
-    console.log(req.files[0].path);
-    res.status(200).send('ok');
-    return true;
-    var token = req.query.token;
+    var token = req.header('Authorisation');
     jwt.verify(token, 'userToken', function(err, decoded){
         if(!err){
-            res.status(200).send(sections_send); //just always sends ok as an example
+            let sectionTitle = req.body.sectionTitle;
+            let sectionText = req.body.sectionText;
+            let sectionFiles = [];
+            for (var i = 0; i < req.files.length; i++) {
+                sectionFiles.push('',req.files[i].originalname);
+            }
+            let fileTitles = req.body.fileTitles;
+            //check if there are any current files asscociated with section
+            //if yes append the new files
+            //set file titles for all files
+            //save all information to the database
+            res.status(200).send('ok');
         } else {
             res.status(403).send(err);
         }
