d5c1019a6b56b99a4f2e722cbd473b71a39a67f1
OliverCrowe <oliver.crowe@hotmail.co.uk>
2020-01-23 16:23:53 +0000

    started implementing multer file uploading

--- routes/api/sections.js
+++ routes/api/sections.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 
+const multer  = require('multer');
+const storage = multer.diskStorage({
+    destination: '../../public',
+    filename: function (req, file, cb) {        
+        // null as first argument means no error
+        cb(null, Date.now() + '-' + file.originalname );
+    }
+});
+var upload = multer({ storage: storage });
+
 const mysql_query = require('../../mysql_query');
 
 class Section { //this is the class for an app section or page
@@ -28,10 +38,12 @@ router.get('/', (req, res) => {
         }
     });
 });
-`
-router.post('/add', (req, res) => {
-    let sections = [new Section(0,'Section 1','Here is some text about S1',0),new Section(1,'Section 2','Here is some text about S2',1)];
-    let sections_send = JSON.stringify(sections);
+
+router.post('/add', upload.array('section_files[]', 20), (req, res) => {
+    let sectionTitle = req.body.sectionTitle;
+    let sectionText = req.body.sectionText;
+    res.status(200).send('ok');
+    return true;
     var token = req.query.token;
     jwt.verify(token, 'userToken', function(err, decoded){
         if(!err){
@@ -40,6 +52,6 @@ router.post('/add', (req, res) => {
             res.status(403).send(err);
         }
     });
-});`;
+});
 
 module.exports = router;
