028505aaad32a163a57f5b86b9009a218537e100
OliverCrowe <oliver.crowe@hotmail.co.uk>
2020-01-24 14:40:10 +0000

    protected files and added eslint file for api code

--- app.js
+++ app.js
@@ -5,13 +5,42 @@ const path = require('path');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/api/login');
 const sectionsRouter = require('./routes/api/sections');
 
+//file protecting adapted from https://stackoverflow.com/questions/11910956/how-to-protect-a-public-dynamic-folder-in-nodejs
+function userIsAllowed(req, callback) {
+    var token = req.query.token;
+    jwt.verify(token, 'userToken', function(err, decoded){
+        if(!err){
+            callback(true);
+        } else {
+            callback(false);
+        }
+    });
+};
+// This function returns a middleware function
+var protectPath = function(regex) {
+return function(req, res, next) {
+    if (!regex.test(req.url)) { return next(); }
+
+    userIsAllowed(req, function(allowed) {
+    if (allowed) {
+        next(); // send the request to the next handler, which is express.static
+    } else {
+        res.end('You do not have permission to view this file!');
+    }
+    });
+};
+};
+
 const app = express();
 const PORT = 3000;
+
+app.use(protectPath(/^\/files\/.*$/));
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
