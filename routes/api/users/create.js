cc1e5cb4e0ceb36bf3bf98649ebbdc6f74206b5c
OliverCrowe <oliver.crowe@hotmail.co.uk>
2020-02-11 12:15:43 +0000

    fixed a bug in uniqueness detection

--- routes/api/users/create.js
+++ routes/api/users/create.js
@@ -30,13 +30,13 @@ router.post('/', (req, res) => {
                 var unique = false;
                 while(!unique) {
                     var username = utils.randomUsername();
-                    if (!(username in names)) { //the username is unique
+                    if (names.indexOf(username) == -1) { //the username is unique
                         unique = true;
+                        const queryString = 'INSERT INTO users (real_name, username, password, password_salt, is_admin) VALUES (?,?,?,?,?)';
+                        const queryArray = [realName, username, hashed_pass, salt, isAdmin];
+                        utils.mysql_query(res, queryString, queryArray, (results, res) => {res.status(200).send(password);});
                     } 
                 }
-                const queryString = 'INSERT INTO users (real_name, username, password, password_salt, is_admin) VALUES (?,?,?,?,?)';
-                const queryArray = [realName, username, hashed_pass, salt, isAdmin];
-                utils.mysql_query(res, queryString, queryArray, (results, res) => {res.status(200).send(password);});
             });
         });
     } catch (err) {
