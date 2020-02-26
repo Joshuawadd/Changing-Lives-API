75f9d8381c10445a1f39be84550db0948c6bb4c8
AnikaitG <44556565+AnikaitG@users.noreply.github.com>
2020-02-26 17:18:43 +0000

    add childRole

--- routes/api/forums/child/list.js
+++ routes/api/forums/child/list.js
@@ -29,18 +29,36 @@ router.get('/', (req, res) => {
                 if (err) throw err;
             });
 
+            function getChildRole(results){
+                for (var child of results) {
+                    if (child.is_admin == 1 && child.is_creator == 1) {
+                        child.childRole = 'staffcreator';
+                    } else if (child.is_admin == 1) {
+                        child.childRole = 'staff';
+                    } else if (child.is_creator == 1) {
+                        child.childRole = 'creator';
+                    } else {
+                        child.childRole = 'user';
+                    }
+                    delete child.is_admin;
+                    delete child.is_creator;
+                }
+            }
+
             function getList(){
                 return new Promise((resolve, reject) => {
                     connection.query(`SELECT IF(is_admin = 1, true, false) AS is_admin FROM users WHERE user_id = ${userId}`, [], (err, admin) => {
                         if (err) throw res.sendStatus(400);
                         if(admin[0].is_admin) {
-                            connection.query(`SELECT c.child_id, c.child_comment, u.username FROM child_comments c INNER JOIN users u ON c.user_id = u.user_id WHERE c.parent_id = ${parentId}`, [], (err, results) => {
+                            connection.query(`SELECT c.child_id, c.child_comment, u.username, IF(u.is_admin = 1, true, false) AS is_admin, IF(p.user_id = c.user_id, true, false) AS is_creator FROM child_comments c INNER JOIN users u ON c.user_id = u.user_id INNER JOIN parent_comments p ON c.parent_id = p.parent_id  WHERE c.parent_id = ${parentId}`, [], (err, results) => {
                                 if (err) throw res.sendStatus(400);
+                                getChildRole(results);
                                 resolve(results);
                             });
                         } else {
-                            connection.query(`SELECT child_id, child_comment FROM child_comments WHERE parent_id = ${parentId}`, [], (err, results) => {
+                            connection.query(`SELECT c.child_id, c.child_comment, IF(u.is_admin = 1, true, false) AS is_admin, IF(p.user_id = c.user_id, true, false) AS is_creator FROM child_comments c INNER JOIN users u ON c.user_id = u.user_id INNER JOIN parent_comments p ON c.parent_id = p.parent_id  WHERE c.parent_id = ${parentId}`, [], (err, results) => {
                                 if (err) throw res.sendStatus(400);
+                                getChildRole(results);
                                 resolve(results);
                             });
                         }
