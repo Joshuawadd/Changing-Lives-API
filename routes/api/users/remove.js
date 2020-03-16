21fe251552b455aa9a1a3196de7ff55c3bf417c7
OliverCrowe <oliver.crowe@hotmail.co.uk>
2020-03-16 15:25:07 +0000

    last admin user cant be deleted now

--- routes/api/users/remove.js
+++ routes/api/users/remove.js
@@ -18,13 +18,21 @@ router.post('/', (req, res) => {
             const userId = req.body.userId;
 
             if (!isNaN(userId)) {
-                const queryString = 'DELETE FROM users WHERE user_id = ?';
-                const queryArray = [userId];
-                utils.mysql_query(res,'SELECT * FROM users WHERE user_id = ?',[userId], (results, res) => {
-                    let userRemove = JSON.stringify({"id": results[0].user_id, "realName": results[0].real_name, "name": results[0].username, "password": results[0].password, "passwordSalt": results[0].password_salt, "isAdmin": results[0].is_admin.readUInt8()});
-                    utils.mysql_query(res, queryString, queryArray, (results, res) => {
-                        utils.log(editor, utils.actions.REMOVE, utils.entities.USER, null, userRemove);
-                        res.sendStatus(200);
+                utils.mysql_query(res,'SELECT * FROM users WHERE is_admin = 1',[], (adminUsers, res) => {
+                    if (adminUsers.length == 1) {
+                        if (userId == adminUsers[0].user_id) {
+                            res.status(400).send('Cannot delete the last admin user!');
+                            return;
+                        }
+                    }
+                    const queryString = 'DELETE FROM users WHERE user_id = ?';
+                    const queryArray = [userId];
+                    utils.mysql_query(res,'SELECT * FROM users WHERE user_id = ?',[userId], (results, res) => {
+                        let userRemove = JSON.stringify({"id": results[0].user_id, "realName": results[0].real_name, "name": results[0].username, "password": results[0].password, "passwordSalt": results[0].password_salt, "isAdmin": results[0].is_admin.readUInt8()});
+                        utils.mysql_query(res, queryString, queryArray, (results, res) => {
+                            utils.log(editor, utils.actions.REMOVE, utils.entities.USER, null, userRemove);
+                            res.sendStatus(200);
+                        });
                     });
                 });
             }
