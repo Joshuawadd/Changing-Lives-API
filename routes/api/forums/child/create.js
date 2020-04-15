29297633dfaefe9160b734d1f9dc4505b0bbab79
Cal-Turasz <callum.turasz@gmail.com>
2020-04-15 08:41:36 +0100

    comments for child forums

--- routes/api/forums/child/create.js
+++ routes/api/forums/child/create.js
@@ -3,16 +3,18 @@ const router = express.Router();
 const utils = require('../../../../utils');
 const Joi = require('joi');
 
+// A function to take a web request response's body and check it contains the required arguments
 function validate(req) {
     const schema = {
-        parentId: Joi.number().integer().min(0).max(2147483647).required(),
-        childComment: Joi.string().max(65535).required()
+        parentId: Joi.number().integer().min(0).max(2147483647).required(), //Check the parentId is an integer within a given range exists
+        childComment: Joi.string().max(65535).required() //Check childComment is a string with a max length exists
     };
-    return Joi.validate(req, schema);
+    return Joi.validate(req, schema); //Validate the body against the schema, if it is valid, return true, else false
 }
 
 router.post('/', (req, res) => {
 
+    // Get the validation response, if it failed, send a bad request status with an error message
     const {error} = validate(req.body);
     if (error) {
         const errorMessage = error.details[0].message;
@@ -20,34 +22,44 @@ router.post('/', (req, res) => {
         return;
     }
 
-    try{
+    try {
 
+        //Take a user's token and verify they have access for this action
         function verify() {
             return new Promise((resolve) => {
                 resolve(utils.tokenVerify(req.header('Authorization')), false);
             });
         }
 
+        //Verify the token
         verify().then((userId) => { // should get the userId from token
+
+            //Given token gave a bad userId meaning the user did not have the permission
             if (!userId) {
                 console.log('can\'t verify with the userId');
                 res.sendStatus(403);
                 return;
             }
-            //console.log(userId);
+
             const parentId = req.body.parentId;
             const childComment = req.body.childComment;
 
+            //SQL query to insert the new comment into the databse
             const queryString = 'INSERT INTO child_comments (user_id,parent_id,child_comment) VALUES (?,?,?)';
 
+            // Using out query unit, pass query string and the data to pass into the query string
             utils.mysql_query(res, queryString, [userId, parentId, childComment], (results, res) => {
-                //const newData = {childId: results.insertId, childComment: childComment};
-                //const newDataLog = JSON.stringify(newData)
+
+                //Log the users attempt at this action whether it failed or was successful
                 utils.log(userId, utils.actions.CREATE, utils.entities.CHILD, null, JSON.stringify({"name": childComment}));
+
+                //Return a success status with the result
                 res.status(201).send(JSON.stringify(results.insertId));
             });
 
         });
+
+        //Something failed, handle it and return internal error occured
     } catch (err) {
         console.log(err);
         res.sendStatus(500);
--- routes/api/forums/child/list.js
+++ routes/api/forums/child/list.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const utils = require('../../../../utils');
 const Joi = require('joi');
 
+//Similar documentation noted in routes/forums/child/create
 function validate(req) {
     const schema = {
         parentId: Joi.number().integer().min(0).max(2147483647).required(),
@@ -28,19 +29,25 @@ function getChildRole(results) {
 }
 
 router.get('/', (req, res) => {
+
+    //Similar documentation noted in routes/forums/child/create
     const {error} = validate(req.query);
     if (error) {
         const errorMessage = error.details[0].message;
         res.status(400).send(errorMessage);
         return;
     }
+
+
     try {
+        //Similar documentation noted in routes/forums/child/create
         function verify() {
             return new Promise((resolve) => {
                 resolve(utils.tokenVerify(req.query.token), false);
             });
         }
 
+        //Similar documentation noted in routes/forums/child/create
         verify().then((userId) => { // should get the userId from token
             if (!userId) {
                 console.log('can\'t verify with the userId');
@@ -55,18 +62,15 @@ router.get('/', (req, res) => {
                 return;
             }
 
-            const queryString = `SELECT IF(is_admin = 1, true, false) AS is_admin FROM users WHERE user_id = ?`
+            //Similar documentation noted in routes/forums/child/create
+            const queryString = `SELECT IF(is_admin = 1, true, false) AS is_admin FROM users WHERE user_id = ?`;
             const queryArray = [userId];
 
             utils.mysql_query(res, queryString, queryArray, (rows, res) => {
                 isAdmin = rows[0].is_admin;
                 const nestedQueryArray = [parentId];
-                var nestedQueryString;
-                //if (isAdmin) {
-                nestedQueryString = `SELECT c.child_id, c.child_comment, u.username, IF(u.is_admin = 1, true, false) AS is_admin, IF(p.user_id = c.user_id, true, false) AS is_creator FROM child_comments c INNER JOIN users u ON c.user_id = u.user_id INNER JOIN parent_comments p ON c.parent_id = p.parent_id  WHERE c.parent_id = ?`
-                //} else {
-                //nestedQueryString = `SELECT c.child_id, c.child_comment, IF(u.is_admin = 1, true, false) AS is_admin, IF(p.user_id = c.user_id, true, false) AS is_creator FROM child_comments c INNER JOIN users u ON c.user_id = u.user_id INNER JOIN parent_comments p ON c.parent_id = p.parent_id  WHERE c.parent_id = ?`
-                //}
+                var nestedQueryString = `SELECT c.child_id, c.child_comment, u.username, IF(u.is_admin = 1, true, false) AS is_admin, IF(p.user_id = c.user_id, true, false) AS is_creator FROM child_comments c INNER JOIN users u ON c.user_id = u.user_id INNER JOIN parent_comments p ON c.parent_id = p.parent_id  WHERE c.parent_id = ?`;
+
                 utils.mysql_query(res, nestedQueryString, nestedQueryArray, (rows, res) => {
                     getChildRole(rows);
                     res.status(200).send(rows);
